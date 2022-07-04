1d4919c0e1fe4575e4816223ed4e53a1c8ec87b8
Isaac Master <isaac_817221mayorix@hotmail.com>
2022-07-04 03:19:33 -0500

    NaVBar creado

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar";   //importamos la funcion y la llamamos en la app
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FaBars,FaTimes} from "react-icons"
+
+const NavBar = () => {
+  return (
+    <div className='flex justify-between items-center w-full h-20 px-4
+     text-white fixed bg-black'>
+      <h1 className='text-5xl'>Isaac Quiroz</h1>
+    </div>
+  )
+}
+
+export default NavBar
