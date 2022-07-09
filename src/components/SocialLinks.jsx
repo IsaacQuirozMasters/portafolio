2b9707b225c422d5149766423b909641d83d88a1
Isaac Master <isaac_817221mayorix@hotmail.com>
2022-07-08 22:42:47 -0500

    fixed error

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaGithub, FagitHub,FaLinkedin} from 'react-icons/fa';
+import {FaGithub,FaLinkedin} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 const SocialLinks = () => {
@@ -50,7 +50,7 @@ const SocialLinks = () => {
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
       <ul>
         {links.map(({id,child,href,style,download}) => (
-            <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-600' + " " +style}>
+            <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-600'+style}>
             <a href={href} 
             className='flex justify-between items-center w-full text-white'
             download={download}
